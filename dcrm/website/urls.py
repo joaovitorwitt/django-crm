bf35a5c4f0b2dbbd635be5bb2ce2b98a9bc6e652
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-05-05 15:06:22 -0300

    fixed login page bug

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-
 # imports views file in the same directory
 from . import views
+from django.urls import path
+from website.views import login_user
 
 app_name = 'website'
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.index, name="home"),
     path("login/", views.login_user, name="login"),
     path("register/", views.register_user, name="register")
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.urls import reverse
 
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request, 'index.html', {})
 
 
+# login page
 def login_user(request):
     if request.method == "POST":
-        ...
+        # get the info the user typed
+        username = request.POST['username']
+        password = request.POST['password']
 
+        # authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been Logged in")
+            return redirect('website:home')
+        else:
+            messages.error(request, "Something went wrong, Try again")
+            return redirect("website:login")
+        
+    # if the method is GET
     else:
         return render(request, 'login.html', {})
 
 
+# register page
 def register_user(request):
     if request.method == "POST":
-        first_name = request.POST['first-name']
-        last_name = request.POST['last-name']
-
-        url = reverse("home")
-
-        return redirect(url, {"first_name": first_name, "last_name": last_name})
+        ...
 
     else:
         return render(request, 'register.html', {})
